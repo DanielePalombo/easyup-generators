7a72e867b93c5f50a2f00e4272ad0b365147e5dd
Alessio Rocco <alessio.rocco.lt@gmail.com>
2010-08-29 15:50:37 +0200

    Created jquery generator

--- lib/easyup-generators.rb
+++ lib/easyup-generators.rb
@@ -1,1 +1,6 @@
 # Easyup-generators
+
+ActionView::Helpers::AssetTagHelper.register_stylesheet_expansion :jquery => ["jquery/jquery-ui.css"]
+ActionView::Helpers::AssetTagHelper.register_javascript_expansion :jquery => ["jquery.min"]
+ActionView::Helpers::AssetTagHelper.register_javascript_expansion :jquery_ui => ["jquery-ui.min"]
+ActionView::Helpers::AssetTagHelper.register_javascript_expansion :jquery_all => ["jquery.min", "jquery-ui.min"]
---
+++ lib/generators/easyup/jquery/jquery_generator.rb
@@ -0,0 +1,25 @@
+module Easyup
+  module Generators
+    class JqueryGenerator < Rails::Generators::Base
+      source_root File.expand_path('../templates', __FILE__)
+
+      class_option :version, :type => :string, :default => 'latest', :desc => "Indicates which JQuery version use"
+      class_option :ui, :type => :boolean, :default => true, :desc => "Indicates when to include JQuery-UI"
+      class_option :ui_version, :type => :string, :default => '1.8.4', :desc => "Indicates which JQuery-UI version use"
+      class_option :theme, :type => :boolean, :default => false, :desc => "Indicates when to Include JQuery default theme (smoothness)"
+
+      def get_jquery
+        get "http://code.jquery.com/jquery-#{options.version}.min.js", "public/javascripts/jquery.min.js"
+      end
+
+      def get_jquery_ui
+         get "http://ajax.googleapis.com/ajax/libs/jqueryui/#{options.ui_version}/jquery-ui.min.js", "public/javascripts/jquery-ui.min.js" if options.ui?
+      end
+
+      def install_jquery_theme
+        directory "smoothness", "public/stylesheets/jquery" if options.theme?        
+      end
+
+    end
+  end
+end
