838239b11e9176b2ca88069d322db99abc28cc4c
Alessio Rocco <alessio.rocco.lt@gmail.com>
2010-08-30 16:26:04 +0200

    Added tmp/ on .gitignore

--- test/config_test.rb
+++ test/config_test.rb
@@ -5,8 +5,14 @@ class ConfigTest < Rails::Generators::TestCase
     destination File.expand_path("../tmp", File.dirname(__FILE__))
     setup :prepare_destination
     
-    test 'config generator' do
+    test 'generator without name' do
       content = run_generator
       assert_file "config/app_config.yml"
+    end
+
+    test 'generator with name' do
+      name = "contact"
+      content = run_generator name
+      assert_file "config/#{name}_config.yml"
     end
   end
